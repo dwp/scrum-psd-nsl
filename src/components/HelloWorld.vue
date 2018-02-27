350fc57c83b0dd904933611012387f024d1ab3f5
petergrainger <peter.grainger1@dwp.gsi.gov.uk>
2018-02-27 10:58:04 +0000

    add content

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -6,7 +6,9 @@
       <h1>Hello, world!</h1>
     </div>
     <div class="row">
-        <div class="col-md-12">Lorem ipsum ...</div>
+        <div class="col-md-12"><h2>ABOUT QA CINEMA</h2><p>
+QA Cinema is the North East’s first futuristic specialised cinema and swimming venue located in the heart of Tyneside River. We opened our doors in 2037 and are the last surviving theatre in the River Tyne, come float and enjoy a film featuring on the new hd Emax!
+QA Cinema welcomes almost 1 billion people into its river every year, offering a cold and chilling programme of film, drinks and water plenty of water.</p></div>
     </div>
 </div>
 </template>
